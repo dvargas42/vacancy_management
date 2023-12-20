c3f111d861d3be609cc0b13d6a6a878c64412ab9
dvargas42 <dvargas.eng@gmail.com>
2023-12-20 19:23:21 -0300

    feat: :sparkles: initial commit

---
+++ src/main/java/br/com/danielvargas/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,15 @@
+package br.com.danielvargas.gestao_vagas.modules.company.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.danielvargas.gestao_vagas.modules.company.entities.CompanyEntity;
+
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID>{
+    Optional<CompanyEntity>  findByUsernameOrEmail(String username, String email);
+
+    Optional<CompanyEntity> findByUsername(String username);
+}
