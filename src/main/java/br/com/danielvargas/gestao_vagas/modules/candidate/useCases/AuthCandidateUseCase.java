9fc77c32257032e7e39a807d2ab3e0e88c543235
dvargas42 <dvargas.eng@gmail.com>
2023-12-27 18:52:59 -0300

    feat: :sparkles: initial commit

---
+++ src/main/java/br/com/danielvargas/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,65 @@
+package br.com.danielvargas.gestao_vagas.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.danielvargas.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.danielvargas.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.danielvargas.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
+            throws AuthenticationException {
+        var candidate = this.candidateRepository
+                .findByUsername(authCandidateRequestDTO.username())
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("username/password incorrect");
+                });
+
+        var passwordMatches = this.passwordEncoder
+                .matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
+        var token = JWT.create()
+                .withIssuer("javagas")
+                .withSubject(candidate.getId().toString())
+                .withClaim("roles", Arrays.asList("CANDIDATE"))
+                .withExpiresAt(expiresIn)
+                .sign(algorithm);
+
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+                .access_token(token.toString())
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
+
+        return authCandidateResponse;
+    }
+}
---
+++ src/main/java/br/com/danielvargas/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,61 @@
+package br.com.danielvargas.gestao_vagas.modules.company.useCases;
+
+import javax.naming.AuthenticationException;
+import java.time.Instant;
+import java.util.Arrays;
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.danielvargas.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.danielvargas.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
+import br.com.danielvargas.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
+            .orElseThrow(() -> {
+                throw new UsernameNotFoundException("Username/password incorrect");
+            });
+
+        boolean passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+        if (!passwordMatches) {
+           throw new UsernameNotFoundException("Username/password incorrect");
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
+        String token = JWT.create().withIssuer("javagas")
+            .withExpiresAt(expiresIn)
+            .withSubject(company.getId().toString())
+            .withClaim("roles", Arrays.asList("COMPANY"))
+            .sign(algorithm);
+
+        var authCompanyResponseDTO = AuthCompanyResponseDTO.builder()
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
+
+        return authCompanyResponseDTO;
+    }
+}
