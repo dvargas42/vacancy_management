517236618c07e481a4e5fff4475174f93c0edb83
dvargas42 <dvargas.eng@gmail.com>
2023-12-28 19:46:05 -0300

    feat: :sparkles: initial commit

---
+++ src/main/java/br/com/danielvargas/gestao_vagas/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package br.com.danielvargas.gestao_vagas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+            .info(new Info().title("Gestão de Vagas").description("API responsável pela gestão de vagas").version("1"))
+            // .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+            // .components(new Components().addSecuritySchemes("Bearer Authentication", createSecurityScheme()));
+            .schemaRequirement("jwt_auth", createSecurityScheme());
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme().name("jwt_auth").scheme("bearer").bearerFormat("JWT").type(SecurityScheme.Type.HTTP).in(SecurityScheme.In.HEADER);
+    }
+    
+}
