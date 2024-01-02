acc4d3cfa48447c786085ad3564ea448d14abbeb
dvargas42 <dvargas.eng@gmail.com>
2024-01-02 14:08:08 -0300

    feat: :sparkles: initial commit

---
+++ src/main/java/br/com/danielvargas/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,13 @@
+package br.com.danielvargas.gestao_vagas.modules.company.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.danielvargas.gestao_vagas.modules.company.entities.JobEntity;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+    List<JobEntity> findByDescriptionContainingIgnoreCase(String filter);
+}
