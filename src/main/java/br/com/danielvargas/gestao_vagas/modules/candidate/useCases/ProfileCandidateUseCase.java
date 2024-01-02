58f4098112e883b6c2697ddd12ac87246c6a93fd
dvargas42 <dvargas.eng@gmail.com>
2024-01-02 14:08:18 -0300

    feat: :sparkles: initial commit

---
+++ src/main/java/br/com/danielvargas/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -0,0 +1,34 @@
+package br.com.danielvargas.gestao_vagas.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.danielvargas.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.danielvargas.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.danielvargas.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
+
+@Service
+public class ProfileCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public ProfileCandidateResponseDTO execute(UUID idCandidate)  {
+        var candidate = this.candidateRepository.findById(idCandidate)
+                .orElseThrow(() -> {
+                    throw new UserNotFoundException();
+                });
+
+        var candidateDTO = ProfileCandidateResponseDTO.builder()
+                    .id(candidate.getId())
+                    .username(candidate.getUsername())
+                    .name(candidate.getName())
+                    .email(candidate.getEmail())
+                    .description(candidate.getDescription())
+                    .build();
+
+        return candidateDTO;
+    }
+}
