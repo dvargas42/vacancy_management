6d77c862781f98fc7167b7e486dee7514fdeaf5a
dvargas42 <dvargas.eng@gmail.com>
2024-01-04 16:25:15 -0300

    feat: :sparkles: initial commit

---
+++ src/main/java/br/com/danielvargas/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,29 @@
+package br.com.danielvargas.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.danielvargas.gestao_vagas.exceptions.CompanyNotFoundException;
+import br.com.danielvargas.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.danielvargas.gestao_vagas.modules.company.repositories.CompanyRepository;
+import br.com.danielvargas.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class CreateJobUseCase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public JobEntity execute(JobEntity jobEntity) {
+        companyRepository.findById(jobEntity.getCompanyId())
+                .orElseThrow(() -> {
+                    throw new CompanyNotFoundException();
+                });
+                
+        return this.jobRepository.save(jobEntity);
+    }
+    
+}
